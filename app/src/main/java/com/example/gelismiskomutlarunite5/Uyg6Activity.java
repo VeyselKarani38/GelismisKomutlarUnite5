9925c245da2b5718d29ee660ce0f2410ddee6bae
BIL-LAB2PC00\Ogrenci <seyydinburhanedin.mtal38@gmail.com>
2023-02-24 10:29:16 +0300

    24.02.2023 saat 10:30

--- app/src/main/java/com/example/gelismiskomutlarunite5/Uyg6Activity.java
+++ app/src/main/java/com/example/gelismiskomutlarunite5/Uyg6Activity.java
@@ -3,19 +3,47 @@ package com.example.gelismiskomutlarunite5;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+
 
 public class Uyg6Activity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.uyg6_activity);
+        EditText editTextkisakenar = findViewById(R.id.kisakenar);
+        EditText editTextuzunkenar = findViewById(R.id.uzunkenar);
+        Button buttonkarealani = findViewById(R.id.karealanı);
+        Button buttondikdortgenalani = findViewById(R.id.dortgenalanı);
+        TextView textViewalan = findViewById(R.id.textalan);
+        buttonkarealani.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int kisaKenar = Integer.parseInt(editTextkisakenar.getText().toString());
+                Uyg6Hesapla sa = new Uyg6Hesapla(kisaKenar);
+                textViewalan.setText("Sonuç = " + sa.deger);
+            }
+        });
+        buttondikdortgenalani.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int uzunKenar = Integer.parseInt(editTextuzunkenar.getText().toString());
+                int kisaKenar = Integer.parseInt(editTextkisakenar.getText().toString());
+                Uyg6Hesapla sa = new Uyg6Hesapla(kisaKenar,uzunKenar);
+                textViewalan.setText("Sonuç = " + sa.deger);
+            }
+        });
     }
 
-    public void btnBack(View view) {
-        Intent uyg = new Intent(Uyg6Activity.this, MainActivity.class);
-        startActivity(uyg);
-    }
+        public void btnBack(View view) {
+            Intent uyg = new Intent(Uyg6Activity.this, MainActivity.class);
+            startActivity(uyg);
+        }
+
 }
---
+++ app/src/main/java/com/example/gelismiskomutlarunite5/Uyg6Hesapla.java
@@ -0,0 +1,11 @@
+package com.example.gelismiskomutlarunite5;
+
+public class Uyg6Hesapla {
+    public int deger;
+    public Uyg6Hesapla(int Kare){
+        deger = Kare * Kare;
+    }
+    public Uyg6Hesapla(int uzunKenar,int KisaKenar){
+        deger = uzunKenar * KisaKenar;
+    }
+}
